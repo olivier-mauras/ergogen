4268dd97e48bf6f49b70ba0d98dd1f2a9bb5b984
Michael van Eerd <mveerd@vcn.nl>
2021-09-03 13:40:21 +0200

    Merge smd-diode branch

--- js/ergogen.js
+++ js/ergogen.js
@@ -916,9 +916,11 @@
 
 	exports.visualize = (points, units) => {
 	    const models = {};
+	    x_unit = units.visual_x || (units.u - 1);
+	    y_unit = units.visual_y || (units.u - 1);
 	    for (const [pname, p] of Object.entries(points)) {
-	        const w = (p.meta.width * units.u) - 1;
-	        const h = (p.meta.height * units.u) - 1;
+	        const w = p.meta.width * x_unit;
+	        const h = p.meta.height * y_unit;
 	        const rect = utils.rect(w, h, [-w/2, -h/2]);
 	        models[pname] = p.position(rect);
 	    }
@@ -1688,7 +1690,9 @@
 	        to: undefined
 	    },
 	    params: {
-	        class: 'D'
+	        class: 'D',
+	        through_hole: true,
+	        via_in_pad: false
 	    },
 	    body: p => `
   
@@ -1716,18 +1720,34 @@
         (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
-    
-        ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
+
+        ${ p.param.via_in_pad ?
+            `
+             ${''/* Vias in SMD pads */}   
+             (pad 1 thru_hole rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${ p.net.to.str })
+             (pad 2 thru_hole rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${ p.net.from.str })
+            `
+        :
+            `
+             ${ ''/* SMD pads on both sides */ }
+             (pad 1 smd rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${ p.net.to.str })
+             (pad 2 smd rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${ p.net.from.str })
+             (pad 1 smd rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${ p.net.to.str })
+             (pad 2 smd rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${ p.net.from.str })
+            `
+        }
         
-        ${''/* THT terminals */}
-        (pad 1 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.from.str})
-        (pad 2 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.to.str})
+        ${ p.param.through_hole === false ?
+            ''
+        :
+            `
+             ${''/* THT terminals */}
+             (pad 1 thru_hole circle (at 3.81 0 ${ p.rot }) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${ p.net.from.str })
+             (pad 2 thru_hole rect (at -3.81 0 ${ p.rot }) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${ p.net.to.str })
+            `
+        }
+
     )
-  
     `
 	};
 
@@ -2926,7 +2946,7 @@
 	};
 
 	var ergogen = {
-	    version: '3.0.0',
+	    version: '3.0.0-mveerd#smd-diode',
 	    process,
 	    inject_footprint: pcbs.inject_footprint
 	};
