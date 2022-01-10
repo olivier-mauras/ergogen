426a60a53e2bfbfba12f0ece1d937802162a3d96
Michael van Eerd <michael@cache.works>
2022-01-10 01:57:33 +0100

    feat (trace): Allow array of points in trace footprint

--- src/footprints/trace.js
+++ src/footprints/trace.js
@@ -2,16 +2,22 @@ module.exports = {
   params: {
     class: 'TRACE',
     side: "F",
-    start: [0, 0],
-    end: [1, 1],
-    width: 0.25
+    points: [[0,0], [10,10]],
+    width: 0.2
   },
-  body: p => `
-    (segment
-      (start ${p.xy(p.param.start[0], p.param.start[1])})
-      (end ${p.xy(p.param.end[0], p.param.end[1])})
+  body: p => p.param.points.map((point, index) => {
+    const toPoint = p.param.points?.[index + 1];
+    if (!toPoint) {
+      return;
+    }
+
+    return (`
+      (segment
+      (start ${p.xy(point[0], point[1])})
+      (end ${p.xy(toPoint[0], toPoint[1])})
       (width ${p.param.width})
       (layer "${p.param.side}.Cu")
-    )
-  `
+      )
+    `).join('\n');
+  })
 }
